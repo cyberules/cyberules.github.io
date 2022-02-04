dc6ccefee71fc20222245df173de3a6f4c324164
Diogo <5861536+dmalopes@users.noreply.github.com>
2022-02-04 12:20:43 +0000

    push-changes

--- js/custom.js
+++ js/custom.js
@@ -119,7 +119,7 @@ $(document).ready(function(){
 
 // ==========  START GOOGLE MAP ========== //
 function initialize() {
-    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
+    var myLatLng = new google.maps.LatLng(40.6699073,-7.9305569);
 
     var mapOptions = {
         zoom: 14,
